caacc8f9a8f847e36ea398dd10a05f178a740c9c
Mads Sørensen <s215805@student.dtu.dk>
2023-02-28 09:08:19 +0100

    defined some movement methods

--- src/Controller/GameController.py
+++ src/Controller/GameController.py
@@ -2,6 +2,7 @@ from src.Model.BoardModel import BoardModel
 
 
 class GameController(object):
+    currentPlayer = 0
 
     def __init__(self):
         self.board = BoardModel()
@@ -9,6 +10,25 @@ class GameController(object):
     def get_owner(self, num):
         return self.board.get_square(num).get_owner()
 
+    def get_input(self):
+        square = input('Select square to move from ')
+        to_move = input('Select square to move to ')
+        print(square, to_move)
+        return
 
+    # Check if the move is valid and if so moves the piece
+    def move(self):
+        return
 
+    def insert_piece(self):
+        return
+
+    def diagonal_move(self):
+        return
+
+    def attack(self):
+        return
+
+    def jump(self):
+        return
 
