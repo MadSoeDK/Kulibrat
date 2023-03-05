0a1f79b6cfaeb259d111df0ccc1cbf9230c3b800
Mads Sørensen <s215805@student.dtu.dk>
2023-03-05 20:38:42 +0100

    removed get_input method

--- src/Controller/GameController.py
+++ src/Controller/GameController.py
@@ -14,12 +14,6 @@ class GameController(object):
         # 0 = Black Player, 1 = Red Player
         self.currentPlayer = self.players[0]
 
-    def get_input(self):
-        square = input('Select square to move from ')
-        to_move = input('Select square to move to ')
-        print(square, to_move)
-        return
-
     # Check if the move is valid and if so moves the piece
     def _is_move_legal(self):
         # Spawn new piece control
