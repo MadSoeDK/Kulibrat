5e72b40088b03dc7af48f77b95b3729c8c452952
Christian <s133288@dtu.dk>
2023-03-07 10:24:20 +0100

    finished method for finding all possible moves need testing

--- src/Controller/MoveController.py
+++ src/Controller/MoveController.py
@@ -33,10 +33,55 @@ def possibleMoves(state: BoardState) -> list[int, (int, int) in range[16]]:
                             break
 
                 # Forward Right and Left move
+                if (i % 3 == 1 or i % 3 == 0) and state.board.squares[i - 2].owner is None:
+                    listOfMoves.insert(moves, (i, i - 2))
+                    moves += 1
+                if (i % 3 == 1 or i % 3 == 2) and state.board.squares[i - 4].owner is None:
+                    listOfMoves.insert(moves, (i, i - 4))
+                    moves += 1
 
             # Reds turn
-            if state.currentPlayer is state.players[1]:
-                None
+            else:
+                # attack move
+                if state.board.squares[i + 3].owner is not state.currentPlayer:
+                    listOfMoves.insert(moves, (i, i + 3))
+                    moves += 1
+                    # Skip move
+                    jumpTo = i
+                    while True:
+                        jumpTo += 3
+                        if jumpTo > 11:
+                            listOfMoves.insert(moves, (i, 14))
+                            moves += 1
+                            break
+                        if state.board.squares[jumpTo].owner is state.currentPlayer:
+                            break
+                        if state.board.squares[jumpTo].owner is None:
+                            listOfMoves.insert(moves, (i, jumpTo))
+                            moves += 1
+                            break
+
+                # Forward Right and Left move
+                if (i % 3 == 1 or i % 3 == 0) and state.board.squares[i + 2].owner is None:
+                    listOfMoves.insert(moves, (i, i + 2))
+                    moves += 1
+                if (i % 3 == 1 or i % 3 == 2) and state.board.squares[i + 4].owner is None:
+                    listOfMoves.insert(moves, (i, i + 4))
+                    moves += 1
+
+
+    # spawn option
+    if pieceOnBoard < 4:
+        if state.currentPlayer is state.players[0]:
+            for i in range(3):
+                if state.board.squares[i + 9].owner is None:
+                    listOfMoves.insert(moves, (13, i + 9))
+                    moves += 1
+        else:
+            for i in range(3):
+                if state.board.squares[i].owner is None:
+                    listOfMoves.insert(moves, (13, i))
+                    moves += 1
 
     finalList = [moves]
     for i in range(16):
