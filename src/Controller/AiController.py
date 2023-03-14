36f18569469d221d56ded8822b88946aac3200c3
Lucas Schoubye <lucasschoubye@gmail.com>
2023-03-14 16:28:59 +0100

    Bugfixed layered_graph and heuristics

--- src/Controller/AiController.py
+++ src/Controller/AiController.py
@@ -1,12 +1,8 @@
 import copy
 from queue import PriorityQueue
 
-import self as self
-
-from src.Model.BoardModel import BoardModel
 from src.Model.BoardState import Action, Node, BoardState
 from src.Controller.MoveController import possibleMoves
-from src.Model.Player import Player
 
 
 class Problem(object):
@@ -44,17 +40,20 @@ class Problem(object):
 
         return newState
 
-    def action_cost(self, state: BoardState, action: Action, newState: BoardState) -> int:
+    def action_cost(self, num_of_actions: int) -> int:
         return 1
 
 
+def mini_max():
+    self.layered_graph
+
 def expand(problem: Problem, node: Node):
     state = node.state
-    for action in problem.actions(state):
+    actions = problem.actions(state)
+    for action in actions:
         new_state = problem.result(state, action)
-        cost = node.path_cost + problem.action_cost(state, action, new_state)
+        cost = node.path_cost + problem.action_cost(len(actions))
         yield Node(state=new_state, parent=node, action=action, path_cost=cost)
-
 
 def best_first_search(problem: Problem):
     maxNodesSearched = 6
@@ -62,17 +61,11 @@ def best_first_search(problem: Problem):
     problem.layered_graph.append(list())
     frontier = PriorityQueue()
     frontier.queue.append(node)
-    reached = {problem.init_state: node}
     for x in range(maxNodesSearched):
         node = frontier.queue.pop()
         problem.layered_graph[len(problem.layered_graph) - 1].append(node)
-        if problem.is_goal(node.state):
-            return node
         for child in expand(problem, node):
-            state = child.state
-            problem.layered_graph[len(problem.layered_graph)-1].append(child)
-            if state not in reached or child.path_cost < reached[state].path_cost:
-                reached[state] = child
-                frontier.queue.append(child)
+            problem.layered_graph[len(problem.layered_graph) - 1].append(child)
+            frontier.queue.append(child)
 
         problem.layered_graph.append(list())
