e9d036556348aefeb4a5bf9e5efb56d941bb0bb2
Christian Andersen <s133288@dtu.dk>
2023-03-16 20:00:59 +0100

    updating AI

--- src/AI/Pruning.py
+++ src/AI/Pruning.py
@@ -5,29 +5,39 @@ from src.Controller.MoveController import possibleMoves
 from src.Model.BoardState import BoardState, Action
 from src.Model.Player import Player
 
+my_dict = dict()
 
 def pruning_start(state: BoardState, desired_player_index: int) -> Action:
     moves = possibleMoves(state)
     best_action: Action
     best_value = float('-inf')
     for move in moves:
-        value = pruning(result(state, move), 1, desired_player_index)
+        value = pruning(result(state, move), 1, desired_player_index, eval_state(state))
         if value > best_value:
             best_action = move
             best_value = value
     return best_action
 
 
-def pruning(state: BoardState, dept, desired_player_index: int) -> float:
+def pruning(state: BoardState, dept, desired_player_index: int, initial_state_value: float) -> float:
+    # Max dept to check
     if dept == 7:
         return eval_state(state)
+
     moves = possibleMoves(state)
     calc = float('-inf') if state.players.index(state.currentPlayer) == desired_player_index else float('inf')
+
+    # The player have no legal moves
+    if not moves:
+        new_state = copy.deepcopy(state)
+        new_state.currentPlayer = new_state.players[0] if new_state.currentPlayer is new_state.players[1] else new_state.players[1]
+        pruning(new_state, dept + 1, desired_player_index, initial_state_value)
+
     for move in moves:
         if state.players.index(state.currentPlayer) == desired_player_index:
-            calc = max(calc, pruning(result(state, move), dept + 1, desired_player_index))
+            calc = max(calc, pruning(result(state, move), dept + 1, desired_player_index, initial_state_value))
         else:
-            calc = min(calc, pruning(result(state, move), dept + 1, desired_player_index))
+            calc = min(calc, pruning(result(state, move), dept + 1, desired_player_index, initial_state_value))
     return calc
 
 
@@ -43,24 +53,6 @@ def eval_state(state: BoardState) -> float:
         if state.board.squares[i].owner is state.players[1]:
             score += score_row[math.floor(i / 3)]
     return score
-
-
-"""
-def new_state(state: BoardState, action: Action):
-    new_board = BoardModel()
-    for i in range(12):
-        new_board.squares[i].owner = state.board.squares[i].owner
-    new_board.squares[state.board.squares.index(action.fromSquare)].owner = None
-    new_board.squares[state.board.squares.index(action.toSquare)].owner = state.currentPlayer
-    new_players = [Player("black"), Player("red")]
-    for i in range(2):
-        new_players[i].points = state.players[i]
-    if state.board.squares.index(action.toSquare) in (12, 13):
-        player_index = state.players.index(state.currentPlayer)
-        new_players[player_index].points += 1
-    new_current_player = state.players[0] if state.currentPlayer is state.players[1] else state.players[0]
-    return BoardState(new_board, state.players, new_current_player)
-"""
 
 
 def result(state: BoardState, action: Action) -> BoardState:
@@ -88,3 +80,4 @@ def result(state: BoardState, action: Action) -> BoardState:
     newState.board.squares[13].owner = None
 
     return newState
+
